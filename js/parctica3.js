e0819ca29d6db79881435f13395d1fc3326ec2a6
Claudia Rivas <usr3860@salesianos-lacuesta.net>
2024-04-01 14:09:57 +0100

    ultima modificacion

--- js/parctica3.js
+++ js/parctica3.js
@@ -67,12 +67,11 @@ while (condition) {
   }
 } 
 
-// Funcion cuantosPares
+/**
+ * howManyPairs()
+ * In this function we are going to calculate how many even numbers are contained in an array
+ */
 function howManyPairs() {
-  /*
-  * FUNCTION HOW MANY PAIRS
-  * In this function we are going to calculate how many even numbers are contained in an array
-  */
   let numPairs = 0;
   numerosA.forEach( element => {
     if (element % 2 == 0) {
@@ -82,11 +81,12 @@ function howManyPairs() {
   window.alert("El nº de pares es :" + numPairs );
 }
 
+/**
+ * biggerNumber()
+ * In this function according to the "numerosB" arrangement we will see which is the largest number
+ * 
+ */
 function biggerNumber() {
-  /*
-  * FUNCTION BIGGER NUMBER
-  * In this function according to the "numerosB" arrangement we will see which is the largest number
-  */
   let biggerNumber = numerosB[0];
   numerosB.forEach( element => {
     if (biggerNumber < element) {
@@ -96,11 +96,12 @@ function biggerNumber() {
   window.alert("El nº mayor es : " + biggerNumber );
 }
 
+/**
+ * half(array)
+ * In this function, when passing an array, we will see what the average of this is.
+ * @param {elements[]} array type int
+ */
 function half(array) {
-  /*
-  * FUNCTION HALF
-  * In this function, when passing an array, we will see what the average of this is.
-  */
   let elementsTotals = array.length;
   let sumArray = 0;
   array.forEach( element => {
@@ -110,11 +111,13 @@ function half(array) {
   window.alert("La media es : " + half );
 }
 
+/**
+ * oddSum(array)
+ * In this function, when passing an array we will make a sum based on the odd elements
+ * @param {elements[]} array type int
+ */
 function oddSum(array) {
-  /*
-  * FUNCTION ODD SUM
-  * In this function, when passing an array we will make a sum based on the odd elements
-  */
+
   let sumElementsOdd = 0;
   array.forEach( element => {
     if(element % 2 != 0){
@@ -124,11 +127,13 @@ function oddSum(array) {
   window.alert("El total de impares es : " + sumElementsOdd );
 }
 
+/**
+ * divisibles(array, number)
+ * This function, based on a number passed to it by the user, will calculate which numbers are divisible based on the number passed by the user.
+ * @param {elements[]} array type int
+ * @param {int} number int
+ */
 function divisibles(array, number) {
-  /*
-  * FUNCTION DIVISIBLES
-  * This function, based on a number passed to it by the user, will calculate which numbers are divisible based on the number passed by the user.
-  */
   let numberDivisible = [];
   array.forEach( element => {
     if(element % number == 0){
@@ -138,12 +143,13 @@ function divisibles(array, number) {
   window.alert("Los numeros divisibles por " + number + " son :\n " + numberDivisible);
 }
 
+/**
+ * increment(array, porcentage)
+ * This function, based on a percentage that the user entered, that percentage will be added to an array that we pass to the function
+ * @param {elements[]} array type int
+ * @param {float} porcentage number porcentage
+ */
 function increment(array, porcentage) {
-  /*
-  * FUNCTION INCREMENT
-  * This function, based on a percentage that the user entered, that percentage will be added to an array that we pass to the function
-  */
-
   porcentage = porcentage/100;
   let auxiliarArray = array; // I am declaring an auxiliary variable to save an array of constant type (numerosA) and that can be modified.
   let newArrayPorcentage = [];
@@ -153,11 +159,14 @@ function increment(array, porcentage) {
   console.log(newArrayPorcentage);
 }
 
+
+/**
+ * product(array1, array2)
+ * In this function we will pass arrays where we will multiply the elements of the two arrays 
+ * @param {elements[]} array1 type int
+ * @param {elements[]} array2 type int
+ */
 function product(array1, array2) {
-  /*
-  * FUNCTION PRODUCT
-  * In this function we will pass arrays where we will multiply the elements of the two arrays
-  */
   let multiplicationArrays = [];
   array1.forEach( i => {
     array2.forEach( j => {
@@ -167,22 +176,24 @@ function product(array1, array2) {
   console.log(multiplicationArrays);
 }
 
+/**
+ * foreInsert(array)
+ * In this function we will insert an array to the array called "numerosB"
+ * @param {elements[]} array type int
+ */
 function foreInsert(array) {
-  /*
-  * FUNCTION FORE INSERT
-  * In this function we will insert an array to the array called "numerosB"
-  */
   array.forEach( x => {
     numerosB.unshift(x);
   });
   console.log(numerosB);
 }
 
+/**
+ * sumTable(table)
+ * In this function we sum all the elements of a matrix, or an array of dimensions
+ * @param {elements[]} table type int
+ */
 function sumTable(table) {
-  /*
-  * FUNCTION SUM TABLE
-  * In this function we sum all the elements of a matrix, or an array of dimensions
-  */
   let result = 0;
   for(let i = 0; i < table.length; i++){
     for (let j = 0; j < table[i].length; j++) {
@@ -190,14 +201,15 @@ function sumTable(table) {
     }
   }
   console.log("la suma es: " + result); 
-
 }
 
+/**
+ * sumRow(table, row)
+ * In this function we will sum the elements of a row
+ * @param {elements[]} table type int
+ * @param {number} row number row of array 
+ */
 function sumRow(table, row) {
-  /*
-  * FUNCTION BIGGER NUMBER
-  * In this function we will sum the elements of a row
-  */
   let result = 0;
   for (let i = 0; i < table[row].length; i++) {
     result += table[row][i];    
